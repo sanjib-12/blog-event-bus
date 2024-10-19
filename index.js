38176a19ba3460e03bdfcdae8618e4fda3a9066a
sanjib <sanjibbarakoti112@gmail.com>
2024-10-19 17:48:30 -0400

    added event-bus functionalities

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-app.post("/events", (req, res) => {
+const events = [];
+
+app.post("/events", async (req, res) => {
    const event = req.body;
+   events.push(event);
 
-   axios.post("http://localhost:4000/events", event);
-   axios.post("http://localhost:4001/events", event);
-   axios.post("http://localhost:4002/events", event);
+   try {
+      await axios.post("http://localhost:4000/events", event);
+      await axios.post("http://localhost:4001/events", event);
+      await axios.post("http://localhost:4002/events", event);
+      await axios.post("http://localhost:4003/events", event);
 
+   } catch (error) {
+      console.error("Error sending events", error);
+   }
    res.send({ status: "OK" });
 });
+
+app.get('/events',(req, res) => {
+   res.send(events);
+})
 
 const port = 4005;
 app.listen(port, () => {
