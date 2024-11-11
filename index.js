853c206f7197d30b0f1229dbe0868f964dcecc87
sanjib <sanjibbarakoti112@gmail.com>
2024-11-11 11:48:33 -0500

    updated the commit

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.post("/events", async (req, res) => {
    events.push(event);
 
    try {
-      await axios.post("http://localhost:4000/events", event);
-      await axios.post("http://localhost:4001/events", event);
-      await axios.post("http://localhost:4002/events", event);
-      await axios.post("http://localhost:4003/events", event);
+      await axios.post("http://posts-clusterip-srv:4000/events", event);
+      await axios.post("http://comments-srv:4001/events", event);
+      await axios.post("http://query-srv:4002/events", event);
+      await axios.post("http://moderation-srv:4003/events", event);
 
    } catch (error) {
       console.error("Error sending events", error);
